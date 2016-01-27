fd39803b26cf32b3cf496693f3a12dc90711fea9
Lupi S <lupinetine@gmail.com>
2016-01-27 15:59:56 -0600

    next set, stopped on mendel's first

--- gc.py
+++ gc.py
@@ -1,4 +1,6 @@
 from Bio import SeqIO
+#takes a fasta file and parses the sequences 
+#returns a dictionary file with the sequence ID as key, sequence as value
 def parse_fasta(fasta_file):
 	fasta_dict = {}
 	fasta_sequences = SeqIO.parse(open(fasta_file), 'fasta')
@@ -8,20 +10,38 @@ def parse_fasta(fasta_file):
 	return fasta_dict
 
 
-
+#given a dna string, return percentage of string that is G or C
 def gc_content(dna_string):
-	full_count = float(dna_string.count("G")) + float(dna_string.count("C"))
-	percentage = full_count / len(dna_string)
+	# count all instances of G and C
+	full_count = dna_string.count("G") + dna_string.count("C")
+	# convert into a percentage. cast to float due to python int math
+	percentage = (float(full_count) / len(dna_string))*100
 	return percentage
 
 
-def gc(file):
-	bp_dict = parse_fasta(file)
-	percentage_dict = bp_dict.copy()
-	for x in bp_dict.keys():
-		percentage_dict[x] = gc_content(bp_dict.get(x))
+def perc_conv(dict):
+	percentage_dict = dict.copy()
+	for x in dict.keys():
+		percentage_dict[x] = gc_content(dict.get(x))
 	return percentage_dict
 
+#given a dictionary of percentages, return key of highest
+def highest_perc(dict):
+	tmp_list = dict.values()
+	tmp_list.sort()
+	return tmp_list[-1]
 
-print gc('rosalind_gc_example.txt')
+def gc(file):
+	bp_dict = parse_fasta(file)
+	new_dict = perc_conv(bp_dict)
+	tmp = highest_perc(new_dict)
+	val = {}
+	for key in new_dict.keys():
+		if new_dict[key] == tmp :
+			val = (key, round(new_dict[key], 6))
+
+	return val
+
+
+print gc('rosalind_gc.txt')
 	
