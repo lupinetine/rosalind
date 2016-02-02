bc0d2a0daeb26492c692fc71fa7f0e9fe9fea1d7
Lupi <lupinetine@gmail.com>
2016-02-02 08:21:03 -0600

    iprb done, revisit later

--- iprb.py
+++ iprb.py
@@ -1,3 +1,5 @@
+from decimal import *
+
 def pop2list(k,m,n):
 	pop_list = list()
 	for i in range(k):
@@ -10,7 +12,7 @@ def pop2list(k,m,n):
 
 def make_pairs(pop):
 	len_pop = len(pop)
-	print len_pop
+	#print len_pop
 	pair_list = list()
 	if len_pop < 2:
 		return "Unable to process populations less than 2"
@@ -19,11 +21,29 @@ def make_pairs(pop):
 			pair_list.append(pop[i]+pop[j]) 
 	return pair_list
 
+def pair_perc(pair):
+	switcher = {
+		"kk":1,
+		"km":1,
+		"kn":1,
+		"mm":.75,
+		"mn":.50,
+		"nn":0
+	}
+	return switcher.get(pair, "nothing")
 
+def perc_list(pairs):
+	perc = list()
+	for i in pairs:
+		perc.append(pair_perc(i))
+	return perc
 
 def iprb(k,m,n):
-	p_list = pop2list(k,m,n)
-	pairs = make_pairs(p_list)
-	print pairs
+	pairs = make_pairs(pop2list(k,m,n))
+	p_list = perc_list(pairs)
+	for i in range(len(p_list)):
+		p_list[i] = Decimal(p_list[i]) * (Decimal(1)/Decimal(len(p_list)))
+	return sum(p_list)
 
-print iprb(2,2,2)
+
+print iprb(15,30,22)
